efd844e633f08999c126fbb224d8a75956774bd4
Ryan Do <falcsmike7@gmail.com>
2017-11-07 11:20:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -52,4 +52,10 @@ class Trip {
       return driver.id === this.driverId
     })
   }
+
+  passenger() {
+    return store.passengers.find(passenger => {
+      return passenger.id === this.passengerId
+    })
+  }
 }
